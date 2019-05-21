6f90ed9fb8f91fba743cc41353281c436d92002c
dunzhixuan <dunzhixuan@vipkid.com.cn>
2019-05-21 17:17:57 +0800

    feat: 背压更详细的介绍

--- app/src/main/java/com/zhixuan/app/rxjava2_master/FlowableBackPressure.java
+++ app/src/main/java/com/zhixuan/app/rxjava2_master/FlowableBackPressure.java
@@ -1,6 +1,7 @@
 package com.zhixuan.app.rxjava2_master;
 
 import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 
 import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
@@ -10,29 +11,110 @@ import io.reactivex.internal.subscribers.BlockingBaseSubscriber;
 
 public class FlowableBackPressure {
 
-		/*https://www.jianshu.com/p/ceb48ed8719d*/
-		private void flowable(){
-				Flowable flowable = Flowable.create(new FlowableOnSubscribe<Integer>() {
-						@Override
-						public void subscribe(FlowableEmitter<Integer> emitter) throws Exception {
+    public static void main(String[] args){
+        request();
+    }
+  /*1、同步订阅关系和异步订阅关系*/
 
-						}
-				}, BackpressureStrategy.ERROR);
 
-				Flowable flowable1 = Flowable.just("1");
+  /*https://www.jianshu.com/p/ceb48ed8719d*/
+  private void flowable() {
+    Flowable flowable =
+        Flowable.create(
+            new FlowableOnSubscribe<Integer>() {
+              @Override
+              public void subscribe(FlowableEmitter<Integer> emitter) throws Exception {
+                  emitter.requested();
+              }
+            },
+            BackpressureStrategy.BUFFER);
 
-				Subscriber subscriber = new BlockingBaseSubscriber() {
-						@Override
-						public void onNext(Object o) {
+    Flowable flowable1 = Flowable.just("1");
 
-						}
+    Subscriber subscriber =
+        new BlockingBaseSubscriber() {
+          @Override
+          public void onNext(Object o) {}
 
-						@Override
-						public void onError(Throwable t) {
+          @Override
+          public void onError(Throwable t) {}
+        };
 
-						}
-				};
+    Flowable.create(
+            new FlowableOnSubscribe<String>() {
+              @Override
+              public void subscribe(FlowableEmitter<String> emitter) throws Exception {
+                  emitter.requested();
+                  emitter.onNext("事件1");
+                  System.out.println("发送了事件1");
+                  emitter.onNext("事件2");
+                  System.out.println("发送了事件2");
+                  emitter.onNext("事件3");
+                  System.out.println("发送了事件3");
+              }
+            },
+            BackpressureStrategy.ERROR)
+        .subscribe(
+            new Subscriber<String>() {
+              @Override
+              public void onSubscribe(Subscription s) {
+                s.request(2);
+              }
 
-				flowable.subscribe(subscriber);
-		}
+              @Override
+              public void onNext(String s) {
+                  System.out.println("接收了："+ s);
+              }
+
+              @Override
+              public void onError(Throwable t) {}
+
+              @Override
+              public void onComplete() {}
+            });
+
+    // ①、s.request(3);
+    // 作用：决定观察者能够接收多少个事件
+    // 如设置了s.request(3)，这就说明观察者能够接收3个事件（多出的事件存放在缓存区）
+    // 官方默认推荐使用Long.MAX_VALUE，即s.request(Long.MAX_VALUE);
+
+    // ②、BackpressureStrategy.ERROR各个参数的用法 @See <a https://www.jianshu.com/p/ceb48ed8719d /a>
+  }
+
+  private static void request(){
+      Flowable.create(
+              new FlowableOnSubscribe<String>() {
+                  @Override
+                  public void subscribe(FlowableEmitter<String> emitter) throws Exception {
+		                  // 调用emitter.requested()获取当前观察者需要接收的事件数量
+                      emitter.requested();
+                      System.out.println(emitter.requested());
+                      System.out.println("发送了事件1");
+                      emitter.onNext("事件1");
+                      System.out.println("发送了事件2");
+                      emitter.onNext("事件2");
+                      System.out.println("发送了事件3");
+                      emitter.onNext("事件3");
+                  }
+              },
+              BackpressureStrategy.ERROR)
+              .subscribe(
+                      new Subscriber<String>() {
+                          @Override
+                          public void onSubscribe(Subscription s) {
+                              s.request(2);
+                          }
+
+                          @Override
+                          public void onNext(String s) {
+                              System.out.println("接收了："+ s);
+                          }
+
+                          @Override
+                          public void onError(Throwable t) {}
+
+                          @Override
+                          public void onComplete() {}
+                      });
+  }
 }
--- app/src/main/java/com/zhixuan/app/rxjava2_master/ObservableCreate.java
+++ app/src/main/java/com/zhixuan/app/rxjava2_master/ObservableCreate.java
@@ -4,12 +4,15 @@ import org.reactivestreams.Subscription;
 
 import java.util.concurrent.TimeUnit;
 
+import io.reactivex.Flowable;
 import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Action;
+import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Function;
 import io.reactivex.internal.observers.BlockingBaseObserver;
 import io.reactivex.schedulers.Schedulers;
 
@@ -24,7 +27,10 @@ public class ObservableCreate {
     Observable.create(
             new ObservableOnSubscribe<String>() {
               @Override
-              public void subscribe(ObservableEmitter<String> emitter /* ①*/) throws Exception {}
+              public void subscribe(ObservableEmitter<String> emitter /* ①*/) throws Exception {
+                // 保证顺序
+                emitter.serialize();
+              }
             })
         .subscribe(
             new BlockingBaseObserver<String>() {
@@ -35,8 +41,121 @@ public class ObservableCreate {
               public void onError(Throwable e) {}
             });
 
+    Observable.create(
+            new ObservableOnSubscribe<String>() {
+              @Override
+              public void subscribe(ObservableEmitter<String> emitter) throws Exception {}
+            })
+        .subscribe(
+            new Observer<String>() {
+              @Override
+              public void onSubscribe(Disposable d) {
+                // 其中isDisposed()方法用来判断当前订阅是否失效，dispose()方法用来取消当前订阅。
+                // 只有当观察者Observer与可观察对象Observable之间建立订阅关系，并且订阅关系有效时，Observer才能对Observable进行响应。如果Observer在响应Observable的过程中，订阅关系被取消，
+                // 则Observer无法对取消订阅关系之后Observable的行为作出响应。
+                // 运行下面的代码，当Observable接收到第5条数据时，取消订阅关系。
+                d.isDisposed();
+                // 解除订阅关系
+                d.dispose();
+              }
 
+              @Override
+              public void onNext(String s) {}
+
+              @Override
+              public void onError(Throwable e) {}
+
+              @Override
+              public void onComplete() {}
+            });
     // ①将Subscriber改为了ObservableEmitter
+    // ②Disposable : 一次性资源
+    // ③返回不再是Subscription
+
+    Disposable disposable =
+        Observable.just("1")
+            .subscribe(
+                new Consumer<String>() {
+                  @Override
+                  public void accept(String s) throws Exception {
+
+                    //
+                  }
+                });
+
+    Disposable disposable1 =
+        Observable.just("1")
+            .subscribe(
+                new Consumer<String>() {
+                  @Override
+                  public void accept(String s) throws Exception {}
+                },
+                new Consumer<Throwable>() {
+                  @Override
+                  public void accept(Throwable throwable) throws Exception {}
+                },
+                new Action() {
+                  @Override
+                  public void run() throws Exception {}
+                });
+
+    Disposable disposable2 =
+        Observable.just("1")
+            .map(
+                new Function<String, Integer>() {
+                  @Override
+                  public Integer apply(String s) throws Exception {
+                    return null;
+                  }
+                })
+            .subscribe(
+                new Consumer<Integer>() {
+                  @Override
+                  public void accept(Integer integer) throws Exception {}
+                });
+
+    Observable observable = Observable.just("1");
+
+    Disposable disposable3 =
+        observable.subscribe(
+            new Consumer<Integer>() {
+              @Override
+              public void accept(Integer integer) throws Exception {
+                // 这里接收数据项
+              }
+            },
+            new Consumer<Throwable>() {
+              @Override
+              public void accept(Throwable throwable) throws Exception {
+                // 这里接收onError
+              }
+            },
+            new Action() {
+              @Override
+              public void run() throws Exception {
+                // 这里接收onComplete。
+              }
+            });
+
+    Flowable flowable = Flowable.just("1");
+
+    Disposable disposable4 = flowable.subscribe(
+        new Consumer<String>() { // 相当于onNext
+          @Override
+          public void accept(String s) throws Exception {}
+        },
+        new Consumer<Throwable>() { // 相当于onError
+          @Override
+          public void accept(Throwable throwable) throws Exception {}
+        },
+        new Action() { // 相当于onComplete，注意这里是Action
+          @Override
+          public void run() throws Exception {}
+        },
+        new Consumer<Subscription>() { // 相当于onSubscribe
+          @Override
+          public void accept(Subscription subscription) throws Exception {}
+        });
   }
 
   private static void from() {
@@ -62,5 +181,4 @@ public class ObservableCreate {
               public void onComplete() {}
             });
   }
-
 }
